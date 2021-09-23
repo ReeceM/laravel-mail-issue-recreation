b877266dfe4c29f9d1db12fa8871c5433d330ae3
Hernán Martínez Reumann <hmreumann@hotmail.com>
2021-09-23 18:11:59 -0300

    Added offer and mailable

---
+++ app/Models/Offer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'listing_id',
+        'user_id',
+        'offer_amount',
+        'currency',
+        'currency_amount',
+        'exchange_rate',
+        'offer_type',
+        'auction_id',
+        'outcome',
+        'details'
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'listing_offers';
+
+    public function listing()
+    {
+        return $this->belongsTo(Listing::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Notifications/TraditionalOfferUserNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\TraditionalOfferUserMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Models\User;
+use App\Models\Offer;
+
+class TraditionalOfferUserNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $user;
+    protected $offer;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Offer $offer)
+    {
+        $this->user = $user;
+        $this->offer = $offer;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new TraditionalOfferUserMail($this->offer))
+            ->to($notifiable->email);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user' => $this->user,
+            'offer' => $this->offer
+        ];
+    }
+}
